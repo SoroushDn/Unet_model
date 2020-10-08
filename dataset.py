62370e2d2675c52aa7068f810107e1ba034364ba
sdn2018 <soroush.d6@gmail.com>
2020-10-08 16:30:35 -0600

    Upload

--- dataset.py
+++ dataset.py
@@ -20,16 +20,12 @@ def conveter_32channels(img):
                            [64, 128, 192, 64, 0, 128, 192, 64, 128, 128, 192, 64, 64, 192, 64, 128, 0, 128, 192, 192,
                             128, 128, 192, 64, 64, 128, 0, 192, 64, 0, 0, 0]])
     [m, n, k] = np.shape(img)
-    # print(np.shape(img))
     outImg = np.zeros([m, n, 32])
-    # print(np.shape(outImg))
     for i in range(32):
         mymap = np.ones([m, n, k])
         for j in range(3):
             mymap[:, :, j] = matColered[j, i] * mymap[:, :, j]
-            # mymap[:][:][j] = 1*mymap[:][:][j]
         outtmp = (np.ones([m, n, k]) - np.ceil(np.abs(img - mymap) / 255))
-        # print(np.shape(outtmp))
         outImg[:, :, i] = (i) * (outtmp[:, :, 0] * outtmp[:, :, 1] * outtmp[:, :, 2])
     return outImg
 
@@ -50,20 +46,15 @@ class Dataset(Dataset):
     def __getitem__(self, idx):
         inp_img_tmp = cv2.imread(self.dataset_image_dir + self.list_files[idx])
         inp_img_tmp = cv2.cvtColor(inp_img_tmp, cv2.COLOR_BGR2RGB)
-        # plt.figure()
-        # plt.imshow(inp_img_tmp)
-        # plt.show()
         inp_img_tmp = cv2.resize(inp_img_tmp, (388, 388))
         part1 = np.fliplr(inp_img_tmp)
         imgp1 = np.concatenate((part1[:, 388 - 92 - 1:-1], inp_img_tmp, part1[:, 0:92]), axis=1)
         part2 = np.flipud(imgp1)
         inp_img_tmp = np.concatenate((part2[388 - 92 - 1:-1, :], imgp1, part2[0:92, :]))
         images = inp_img_tmp.astype(np.float32)
-        # print(images)
         for j in range(3):
             images[:, :, j] = images[:, :, j] / np.max(images[:, :, j])
-        # print(np.max(lbl_img[i]))
-        print(np.shape(images))
+
         img_input = torch.from_numpy(np.array([images[:, :, 0], images[:, :, 1], images[:, :, 2]]))
 
         lbl_img_tmp = cv2.imread(self.dataset_lbl_dir + self.labels[idx] + '_L.png')
